b52e8ad12596c260975d8ee389af47f89d4236cd
Nibhafathima <151762426+Nibhafathima@users.noreply.github.com>
2024-01-02 20:38:02 +0530

    Add files via upload

--- factorial.py
+++ factorial.py
@@ -1,6 +1,8 @@
-def factorial(n):
-    if n==1:return 1
-    else:return(n* factorial(n-1))
-num=int(input("enter a number")
-if num>=1:
- print("factorial of ,num,factorial(num))
+import math
+def factorial():
+    x=int(input("enter the number:",))
+    fact=math.factorial(x)
+    return fact
+fact=factorial()
+print("the factorial of number:",fact)
+    
